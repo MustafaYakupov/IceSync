f4a89dcdbb2296e6947f76861d0143b106c392b2
MustafaYakupov <mustafa.yakupov97@gmail.com>
2026-01-10 17:49:32 +0200

    Added data constants in Common

---
+++ IceSync.Common/Constants/DataConstants.cs
@@ -0,0 +1,12 @@
+﻿
+namespace IceSync.Common.Constants;
+
+public static class DataConstants
+{
+    public static class Workflow
+    {
+        public const int ApiWorkflowIdMaxLength = 200;
+        public const int WorkflowNameMaxLength = 500;
+        public const int MultiExecBehaviorMaxLength = 200;
+    }
+}
