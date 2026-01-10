296adaa87b26fcf68f63de7a43bfa0fc8f57db6d
MustafaYakupov <mustafa.yakupov97@gmail.com>
2026-01-10 22:34:26 +0200

    Added WorkflowDto

---
+++ IceSync.Infrastructure/UniversalLoader/Models/WorkflowDto.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace IceSync.Infrastructure.UniversalLoader.Models;
+
+public class WorkflowDto
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; } = "";
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = "";
+
+    [JsonPropertyName("isActive")]
+    public bool IsActive { get; set; }
+
+    [JsonPropertyName("multiExecBehavior")]
+    public string? MultiExecBehavior { get; set; }
+}
