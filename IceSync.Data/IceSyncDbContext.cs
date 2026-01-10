e68ee7a4f1207da7b86b7f408a722b82a558a085
MustafaYakupov <mustafa.yakupov97@gmail.com>
2026-01-10 21:27:10 +0200

    Added DbContext

---
+++ IceSync.Data/IceSyncDbContext.cs
@@ -0,0 +1,23 @@
+﻿using IceSync.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IceSync.Data;
+
+public class IceSyncDbContext : DbContext
+{
+    public IceSyncDbContext(DbContextOptions<IceSyncDbContext> options) : base(options) { }
+
+    public DbSet<Workflow> Workflows => Set<Workflow>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Workflow>()
+            .HasIndex(x => x.ApiWorkflowId)
+            .IsUnique();
+
+        modelBuilder.Entity<Workflow>()
+            .HasQueryFilter(x => !x.IsDeleted); // hide deleted by default
+    }
+}
