e9c755d60bb19f39f8dbd5f832708ba2a5f0cb3a
MustafaYakupov <mustafa.yakupov97@gmail.com>
2026-01-10 22:59:41 +0200

    Added API client contract

---
+++ IceSync.Infrastructure/UniversalLoader/Contracts/IUniversalLoaderClient.cs
@@ -0,0 +1,9 @@
+﻿using IceSync.Infrastructure.UniversalLoader.Models;
+
+namespace IceSync.Infrastructure.UniversalLoader.Contracts;
+
+public interface IUniversalLoaderClient
+{
+    Task<IReadOnlyList<WorkflowDto>> GetWorkflowsAsync(CancellationToken ct);
+    Task<bool> RunWorkflowAsync(string workflowId, CancellationToken ct);
+}
