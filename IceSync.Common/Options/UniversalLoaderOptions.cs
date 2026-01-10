7c9b00fc211d6c9d091dd34602cb1e88857fb57c
MustafaYakupov <mustafa.yakupov97@gmail.com>
2026-01-10 17:46:43 +0200

    Added universal loader options in Common

---
+++ IceSync.Common/Options/UniversalLoaderOptions.cs
@@ -0,0 +1,10 @@
+﻿
+namespace IceSync.Common.Options;
+
+public sealed class UniversalLoaderOptions
+{
+    public string BasseApiUrl { get; set; } = "";
+    public string CompanyId { get; set; } = "";
+    public string UserId { get; set; } = "";
+    public string UserSecret { get; set; } = "";
+}
