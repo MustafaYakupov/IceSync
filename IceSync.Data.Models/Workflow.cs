0ce8051408dcd7dfe0c94f8398d0dd992b3b0272
MustafaYakupov <mustafa.yakupov97@gmail.com>
2026-01-10 18:03:32 +0200

    Added Workflow data model

---
+++ IceSync.Data.Models/Workflow.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static IceSync.Common.Constants.DataConstants.Workflow;
+
+
+namespace IceSync.Data.Models;
+
+public class Workflow
+{
+    public Workflow()
+    {
+        this.Id = Guid.NewGuid();
+    }
+
+    [Key]
+    [Comment("Unique Identifier")]
+    public Guid Id { get; set; }
+
+    [Required]
+    [MaxLength(ApiWorkflowIdMaxLength)]
+    [Comment("Universal Loader Workflow unique identifier (from API)")]
+    public string ApiWorkflowId { get; set; } = null!;
+
+    [Required]
+    [MaxLength(WorkflowNameMaxLength)]
+    [Comment("Workflow name")]
+    public string WorkflowName { get; set; } = null!;
+
+    [Comment("Shows whether workflow is active")]
+    public bool IsActive { get; set; }
+
+    [MaxLength(MultiExecBehaviorMaxLength)]
+    [Comment("Multi exec behavior")]
+    public string? MultiExecBehavior { get; set; }
+
+    [Comment("Shows whether Workflow is deleted or not")]
+    public bool IsDeleted { get; set; }
+}
